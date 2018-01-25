b8879b58f3dae05a13473ca9e315c4e0c070a610
arthur silva <arxdsilva@gmail.com>
2018-01-25 13:40:16 -0200

    user: add location structure

---
+++ user.go
@@ -0,0 +1,17 @@
+package draven
+
+type Location struct {
+	City        string
+	CityID      int32
+	Country     string
+	CountryCode string
+	Latitude    float32
+	// LocatedIn ???
+	Longitude float32
+	Name      string
+	Region    string
+	RegionID  int32
+	State     string
+	Street    string
+	ZIP       string
+}
