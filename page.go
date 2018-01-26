9462dc2acb61cb392b90794e1fd0562e3d19ff46
arthur silva <arxdsilva@gmail.com>
2018-01-26 13:58:49 -0200

    page: add atributes and docs

--- page.go
+++ page.go
@@ -1,48 +1,105 @@
 package draven
 
+import "time"
+
+// fazer um metodo do request
+// para o cara receber raw do
+// body da requisicao ao inves
+// dessas structs
+
+// Page is a facebook Page
+// Page https://developers.facebook.com/docs/graph-api/reference/page/
 type Page struct {
-	ID            string
-	About         string
-	AccessToken   string
-	AdCampaign    AdCampaign
-	Affiliation   string
-	AppID         string
-	AppLinks      AppLinks
-	ArtistsWeLike string
-	Attire        string
-	Awards        string
-	BandInterests string
-	// BestPage      Page
-	Bio          string
-	Birthday     string
-	BookingAgent string
-	Built        string
+	ID                              string
+	About                           string
+	AccessToken                     string
+	AdCampaign                      AdCampaign
+	Affiliation                     string
+	AppID                           string
+	AppLinks                        AppLinks
+	ArtistsWeLike                   string
+	Attire                          string
+	Awards                          string
+	BandInterests                   string
+	BestPage                        *Page
+	Bio                             string
+	Birthday                        string
+	BookingAgent                    string
+	Built                           string
+	CanCheckin                      bool
+	CanPost                         bool
+	Category                        string
+	CategoryList                    []PageCategory
+	Checkins                        int32
+	CompanyOverview                 string
+	ContactAddress                  MailingAddress
+	CountryPageLikes                int32
+	Cover                           CoverPhoto
+	CulinaryTeam                    string
+	CurrentLocation                 string
+	Description                     string
+	DescriptionHTML                 string
+	DirectedBy                      string
+	DisplaySubtext                  string
+	DisplayedMessageResponseTime    string
+	Emails                          []string
+	Engagement                      Engagement
+	FanCount                        int32
+	FeaturedVideo                   Video
+	Features                        string
+	FoodStyles                      []string
+	Founded                         string
+	GeneralInfo                     string
+	GeneralManager                  string
+	Genre                           string
+	GlobalBrandPageName             string
+	GlobalBrandRootID               string
+	HasAddedApp                     bool
+	HasWhatsappNumber               bool
+	Hometown                        string
+	Hours                           map[string]string
+	Impressum                       string
+	Influences                      string
+	IsAlwaysOpen                    bool
+	IsChain                         bool
+	IsCommunityPage                 bool
+	IsEligibleForBrandedContent     bool
+	IsOwned                         bool
+	IsPermanentlyClosed             bool
+	IsPublished                     bool
+	IsUnclaimed                     bool
+	IsWebhooksSubscribed            bool
+	LeadgenHasCRMIntegration        bool
+	LeadgenHasFatPingCRMIntegration bool
+	LeadgenTosAcceptanceTime        time.Time
+	LeadgenTosAccepted              bool
+	LeadgenTosAcceptingUser         User
+	Link                            string
+	Location                        Location
+	Members                         string
+	MerchantID                      string
+	MerchantReviewStatus            int
+	MessengerAdsQuickRepliesType    int
+	Mission                         string
+	MPG                             string
+	Name                            string
+	NameWithLocationDescriptor      string
+	Network                         string
+	NewLikeCount                    int32
+	OfferEligible                   bool
+	OverallStarRating               float32
+	PageToken                       string
+	ParentPage                      *Page
+	// MessengerAdsDefaultPageWelcome
+	// LeadgenFormPreviewDetails LeadgenFormPreviewDetails
+	// InstantArticlesReviewStatus int
+	// InstagramBusinessAccount ShadowIGUser
 	// Business
-	CanCheckin      bool
-	CanPost         bool
-	Category        string
-	CategoryList    []PageCategory
-	Checkins        int32
-	CompanyOverview string
 	// ConnectedInstagramAccount
-	ContactAddress MailingAddress
 	// Context OpenGraphContext
-	CountryPageLikes             int32
-	Cover                        CoverPhoto
-	CulinaryTeam                 string
-	CurrentLocation              string
-	Description                  string
-	DescriptionHTML              string
-	DirectedBy                   string
-	DisplaySubtext               string
-	DisplayedMessageResponseTime string
-	Emails                       []string
-	Engagement                   Engagement
-	FanCount                     int32
-	// to be continued...
-	// https://developers.facebook.com/docs/graph-api/reference/page/
 }
 
+// AppLinks https://developers.facebook.com/docs/graph-api/reference/app-links/
 type AppLinks struct {
 	ID string
 	// Android          []AndroidAppLink
@@ -54,24 +111,24 @@ type AppLinks struct {
 	// WindowsPhone     []WindowsAppLink
 	// WindowsUniversal []WindowsAppLink
 	// to be continued ...
-	// https://developers.facebook.com/docs/graph-api/reference/app-links/
 }
+
+// AdCampaign https://developers.facebook.com/docs/marketing-api/reference/ad-campaign/
 type AdCampaign struct {
 	ID        string
 	AccountID string
 	// to be continued ...
-	// https://developers.facebook.com/docs/marketing-api/reference/ad-campaign/
 }
 
 type MailingAddress struct {
-	ID   string
-	City string
-	// CityPage   Page
+	ID         string
+	City       string
 	Contry     string
 	PostalCode string
 	Region     string
 	Street1    string
 	Street2    string
+	CityPage   *Page
 }
 
 type PageCategory struct {
